1a03b8b893c3d3888f852d9c12df3ffcb7c3d608
Ajay Kushwaha <ak383834@gmail.com>
2023-12-01 01:11:10 +0530

    working

---
+++ src/App.jsx
@@ -0,0 +1,42 @@
+import { useState, useEffect } from 'react'
+import '../src/app.scss'
+const url = 'https://api.github.com/users'
+
+function App() {
+  const [users, setUsers] = useState([])
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const data = await fetch(url)
+        const responce = await data.json()
+        setUsers(responce)
+      } catch (error) {
+        console.log(error)
+      }
+    }
+    fetchData()
+  }, [])
+
+  return (
+    <div className='main_container'>
+      <h2>GitHub Authenticated Users:</h2>
+      <div className='container'>
+        {users.map((user) => {
+          const { id, login, avatar_url, html_url, followers_url } = user
+          return (
+            <div className='user' key={id}>
+              <img src={avatar_url} alt={login} />
+              <div className='details'>
+                <h4>{login.toUpperCase()}</h4>
+                <a href={html_url}>GitHub</a>
+                <a href={followers_url}>Followers</a>
+              </div>
+            </div>
+          )
+        })}
+      </div>
+    </div>
+  )
+}
+
+export default App
---
+++ src/assets/components/data.jsx
@@ -0,0 +1,14 @@
+const people = [
+  { id: 1, name: 'Ajay Kushwaha', college: 'Aliah University' },
+  { id: 2, name: 'Sara Smith', college: 'Harvard University' },
+  { id: 3, name: 'John Doe', college: 'MIT' },
+  { id: 4, name: 'Emily Johnson', college: 'Stanford University' },
+  { id: 5, name: 'Raj Patel', college: 'University of California, Berkeley' },
+  { id: 6, name: 'Aisha Khan', college: 'Oxford University' },
+  { id: 7, name: 'Carlos Rodriguez', college: 'ETH Zurich' },
+  { id: 8, name: 'Anna Kim', college: 'Seoul National University' },
+  { id: 9, name: 'Mohammed Ali', college: 'National University of Singapore' },
+  { id: 10, name: 'Isabella Martinez', college: 'University of Sydney' },
+]
+
+export default people
